d33a895ee073fb9f3c741d952e891f7fbafdc0ff
Andriel Ribeiro <andriel_ribeiro@estudante.sesisenai.org.br>
2025-07-10 08:43:21 -0300

    add>Exercicio_3

---
+++ exercicio_3.php
@@ -0,0 +1,14 @@
+<?php
+
+$numero = 10; 
+echo $numero;
+echo "<br>";
+if ($numero > 0)  {
+   echo "Numero positivo.";
+}elseif ($numero <0) {
+echo "Numero negativo.";
+}else {
+    echo "zero.";
+}
+
+?>
