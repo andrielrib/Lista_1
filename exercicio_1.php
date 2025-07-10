f7774d8cc4c2c3b50890048ec40fac4a9c5532e7
Andriel Ribeiro <andriel_ribeiro@estudante.sesisenai.org.br>
2025-07-10 08:32:18 -0300

    add>exercicio_1

---
+++ exercicio_1.php
@@ -0,0 +1,18 @@
+<?php 
+$numero = 10;
+
+if ($numero % 2 == 0) {
+  echo "$numero é um número par.";
+} else {
+  echo "$numero é um número ímpar.";
+}
+
+$numero = 2;
+
+if ($numero % 2 == 0) {
+  echo "$numero é um número par.";
+} else {
+  echo "$numero é um número ímpar.";
+}
+
+?>
